b145159c278f721f5198f92585eeaa300add13b9
novakblanka03 <novakblanka2003@gmail.com>
2024-04-26 19:15:35 +0300

    Eredmenyek felrakva

--- app/ro/rezultate/page.jsx
+++ app/ro/rezultate/page.jsx
@@ -1,29 +1,16 @@
 export default function Page() {
-  return (
-    <main>
-      
-      <h1 className="space-x-2 text-lg font-semibold md:text-2xl">
-        Rezultatele nu sunt disponibile!
-        {/*
-        <span>Rezultate inițiale:</span>
-        <a
-          href="/assets/eredmenyek.pdf"
-          target="_blank"
-          className="font-normal underline"
-        >
-          inițiale.pdf
-        </a>
-      </h1>
-      <h1 className="space-x-2 text-lg font-semibold md:text-2xl">
-        <span>Rezultate finale:</span>
-        <a
-          href="/assets/final.pdf"
-          target="_blank"
-          className="font-normal underline"
-        >
-          finale.pdf
-  </a>*/}
-      </h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1 className="space-x-2 text-lg font-semibold md:text-2xl">
+                <span>Rezultate:</span>
+                <a
+                    href="/assets/final.pdf"
+                    target="_blank"
+                    className="font-normal underline"
+                >
+                    rezultate.pdf
+                </a>
+            </h1>
+        </main>
+    );
 }
